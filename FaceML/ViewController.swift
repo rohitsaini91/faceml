4e66848c1e975dfa9a3edb33bf10c3564569a654
Rohit Saini <sketchlearn7@gmail.com>
2020-03-14 11:23:49 +0530

    Initial Commit

---
+++ FaceML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceML
+//
+//  Created by Rohit Saini on 14/03/20.
+//  Copyright © 2020 Rohit Saini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
